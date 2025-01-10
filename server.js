e472db5c938f0d1bcf878cd8ebf9e9ac58e2dbe6
DanielBorbafs <dev.danielborba@gmail.com>
2025-01-10 03:28:46 -0300

    fix: rota de update/put

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import getRoutes from './routes/get.js';
 import postRoutes from './routes/post.js';
 import deleteRoutes from './routes/delete.js';
+import putRoutes from './routes/put.js';
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use('/', getRoutes);
 app.use('/', postRoutes);
 app.use('/', deleteRoutes);
+app.use('/', putRoutes);
 
 app.listen(3000, () => console.log('Servidor rodando'));
