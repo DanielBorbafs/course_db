a4059112d0591ae485ce867122233e3520f4c0bb
DanielBorbafs <dev.danielborba@gmail.com>
2025-07-18 15:38:11 -0300

    querys

--- db/querys.sql
+++ db/querys.sql
@@ -14,3 +14,21 @@ ON C.id = A.id_curso
 GROUP BY C.titulo
 HAVING (AVG(A.NOTA)) >= 4
 GO
+
+-- Mostre os alunos que nunca avaliaram nenhum curso.
+SELECT U.USERNAME
+FROM USERS U 
+LEFT JOIN AVALIACOES A ON U.ID = A.id_aluno
+WHERE U.ROLE = 'ALUNO'
+AND A.ID_ALUNO IS NULL
+
+
+-- Mostre os professores que criaram mais de 3 cursos.
+SELECT U.USERNAME, COUNT(C.id_professor) AS QTD_CURSOS
+FROM USERS U 
+JOIN CURSOS C 
+ON U.id = C.id_professor
+WHERE U.role = 'professor'
+GROUP BY U.USERNAME
+HAVING COUNT(C.id_professor) > 3
+
