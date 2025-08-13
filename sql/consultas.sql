490351becfb1448f0bc737fc1bcd56e9120b16a1
DanielBorbafs <dev.danielborba@gmail.com>
2025-08-13 20:09:26 -0300

    feat: consultas iniciais 🔍

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -1,71 +1,83 @@
--- QUERY QUE TRAZ O VALOR TOTAL R$ DE LUCRO BRUTO COM PEDIDOS FINALIZADOS
-select sum(valor) as valor_final
-  from (
-   select ( quantidade * preco_unitario ) as valor
-     from itens_pedido
-     join pedidos
-   on itens_pedido.pedido_id = pedidos.id
-    where pedidos.status_pedido = 'Finalizado'
-);
+--- [ Perfil Geral dos Clientes] -----
+/*
+"um relatório com a quantidade total de clientes cadastrados, 
+mostrando também quantos são homens e quantos são mulheres,
+e a média de idade por grupo*/
 
--- VALOR TOTAL DE CUSTO COM TODAS AS CAMPANHAS
-select sum(custo_total) as valor_gasto
-  from campanhas;
-
--- VALOR GASTO POR CANAL DE CAMPANHAS
-select canal,
-       sum(custo_total) as custo_total
-  from campanhas
- group by canal
- order by custo_total desc;
-
-
- -- CANAL DE CAMPANHA COM MAIS CLIQUES
-select c.canal,
-       count(k.campanha_id) as cliques_totais
-  from campanhas c
-  join cliques_campanha k
-on c.id = k.campanha_id
- group by c.canal
- order by cliques_totais desc;
-
-
- -- QUANTIDADE DE CLIENTES 
-select count(id) as quantidade_cliente
+-- QUANTIDADE TOTAL DE CLIENTES
+select count(id) as qtd_clientes
   from clientes;
 
--- QUANTIDADE DE CLIENTES AGRUPADO POR SEXO
+-- QUANTIDADE DE CLIENTES MASCULINOS E FEMININOS
 select sexo,
-       count(id) as quantidade_cliente
+       count(id) as qtd_clientes
   from clientes
  group by sexo;
 
--- DISTRIBUIÇÃO DEMOGRÁFICA
-select uf,
-       count(id) as quantidade_clientes
+-- MÉDIA DE IDADE POR GRUPO
+select sexo,
+       round(
+          avg(trunc(months_between(
+             sysdate,
+             data_nascimento
+          ) / 12)),
+          2
+       ) as media_idade
   from clientes
- group by uf
- order by quantidade_clientes desc;
+ group by sexo;
 
- -- QUANTIDADE TOTAL DE DEVOLUCOES
-select count(id) as contagem
-  from devolucoes;
 
-  -- DISTRIBUIÇÃO POR STATUS DAS ENTREGAS
-select status_entrega,
-       count(id) as pedido_id
-  from entregas
- group by status_entrega;
+--- [ Comportamento de Acesso dos Clientes ] -----
 
- -- TRAZENDO OS PRODUTOS QUE MAIS GERARAM RECEITA (SEM DESCONTO R$)
-select nome_produto,
-       sum(valor_sem_desconto) as valor_total
-  from (
-   select p.nome_produto,
-          ( i.quantidade * i.preco_unitario ) as valor_sem_desconto
-     from itens_pedido i
-     join produtos p
-   on i.produto_id = p.id
-)
- group by nome_produto
- order by valor_total desc;
+/* Liste os 5 produtos mais clicados nas campanhas nos últimos 3 meses, 
+e informe quantos cliques cada um recebeu."
+*/
+select p.nome_produto,
+       count(c.produto_id) as qtd_cliques
+  from cliques_campanha c
+ inner join produtos p
+on p.id = c.produto_id
+ where c.data_clique >= sysdate - 90
+ group by p.nome_produto
+ order by qtd_cliques desc
+ fetch first 5 rows only;
+
+
+
+--- [ Vendas Gerais ] -----
+
+/* Total de vendas e o valor total recebido por canal de venda, 
+considerando apenas pedidos finalizados.
+*/
+
+select p.canal_venda,
+       round(
+          sum(i.quantidade *(i.preco_unitario - i.desconto)),
+          2
+       ) as valor_total
+  from pedidos p
+ inner join itens_pedido i
+on i.pedido_id = p.id
+ where p.status_pedido = 'Finalizado'
+ group by p.canal_venda;
+
+
+--- [ Análise de produtos ] -----
+
+--- [ Fornecedores ] -----
+
+--- [ Reposição de estoque ] -----
+
+--- [ Campanha de marketing ] -----
+
+--- [ Análise de pagamentos ] -----
+
+--- [ Entregas ] -----
+
+--- [ Devoluções ] -----
+
+--- [ Suporte ao cliente ] -----
+
+--- [ Análise Cruzada - Clientes e Pedidos ] -----
+
+--- [ Análise Cruzada - Campanhas e Pedidos ] -----
